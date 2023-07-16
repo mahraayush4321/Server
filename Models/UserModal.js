4822d40f2ee8c501a5b3b521f01bff28331bfc44
mahraayush4321 <tanishqmahra@gmail.com>
2023-07-17 02:02:45 +0530

    envchange

---
+++ Models/UserModal.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const userSchema = new userSchema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
