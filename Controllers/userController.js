742e9ea9c8229f1d900b649d1d4c1dab8c12a04a
mahraayush4321 <tanishqmahra@gmail.com>
2023-09-08 18:54:20 +0530

    dyna

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -7,9 +7,21 @@ export const getUser = async (req, res) => {
     const user = await userModal.findById(id);
 
     if (user) {
-      res.status(200).json(user);
+      const { password, ...other } = user._doc;
+
+      res.status(200).json(other);
+    } else {
+      res.status(404).json("No such user exists");
     }
   } catch (error) {
     res.status(500).json(error);
   }
 };
+
+export const updateUser=async(req,res)=>{
+    const id=req.params.id;
+
+    const{currentUserId,currentUserAdmin,password}=req.body;
+
+    
+}
