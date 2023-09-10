34c2386efd0741aed2fcf05829d1cf480ecaa977
mahraayush4321 <tanishqmahra@gmail.com>
2023-09-10 21:13:06 +0530

    up

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyparser from "body-parser";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import { loadRoutes } from "./Routeload/load.js";
 
 const app = express();
@@ -11,5 +12,14 @@ app.use(bodyparser.urlencoded({ limit: "30mb", extended: true }));
 dotenv.config();
 
 loadRoutes(app);
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch(console.error);
 
 export default app;
--- server.mjs
+++ server.mjs
@@ -1,13 +1,16 @@
 import app from "./app.js";
-import mongoose from "mongoose";
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-  })
-  .then(() => {
-    console.log("db connected");
-    app.listen(process.env.PORT, () => {
-      console.log(`listening to port ${process.env.PORT}`);
-    });
-  })
-  .catch(console.error);
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`listening to port ${process.env.PORT}`);
+});
+
+process.on("SIGTERM", shutDown);
+process.on("SIGINT", shutDown);
+
+function shutDown() {
+  console.log("received signal to shutdown");
+  server.close(() => {
+    console.log("Closed out remaining connections");
+    process.exit(0);
+  });
+}
