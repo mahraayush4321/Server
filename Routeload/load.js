c98138e1bf07123f0be3d4594645e1aa88090f4e
mahraayush4321 <tanishqmahra@gmail.com>
2023-09-12 21:01:55 +0530

    chn

--- Routeload/load.js
+++ Routeload/load.js
@@ -3,7 +3,6 @@ import { readdir } from "fs/promises";
 export async function loadRoutes(app) {
   try {
     const routeFiles = await readdir("./Routes");
-
     for (const file of routeFiles) {
       if (file.endsWith(".mjs")) {
         const routeModule = await import(`../Routes/${file}`);
