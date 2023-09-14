20dd3fae0b1daae960f1601555677218ace63e22
mahraayush4321 <tanishqmahra@gmail.com>
2023-09-14 18:40:42 +0530

    up

--- socket/socket.js
+++ socket/socket.js
@@ -3,7 +3,7 @@ import { Server } from "socket.io";
 export function initSocket(server) {
   const io = new Server(server);
   io.on("connection", (socket) => {
-    console.log("A user connected");
+    console.log(socket.id);
 
     socket.on("chat message", (msg) => {
       io.emit("chat message", msg);
