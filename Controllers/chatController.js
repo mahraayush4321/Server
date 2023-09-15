f32e4e7176bc994a0da3096f1bee8345e1f615d3
mahraayush4321 <tanishqmahra@gmail.com>
2023-09-15 23:12:08 +0530

    chng

--- Controllers/chatController.js
+++ Controllers/chatController.js
@@ -4,4 +4,10 @@ export const createChats = async (req, res) => {
   const newChat = new chatModel({
     users: [req.body.senderId, req.body.receiverId],
   });
+  try {
+    const result = newChat.save();
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
