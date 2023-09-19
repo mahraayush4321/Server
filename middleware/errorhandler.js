aeabfe0805e63dc63ed7056b8096f19547bc6b43
mahraayush4321 <tanishqmahra@gmail.com>
2023-09-19 17:32:48 +0530

    chngs

--- middleware/errorhandler.js
+++ middleware/errorhandler.js
@@ -1,4 +1,5 @@
-const errorHandler = (error, req, res, next) => {
+export const errorHandler = (error, req, res, next) => {
+  console.log("Error handled by errorHandler middleware:", error);
   error.status = error.status || 500;
   error.message = error.message || "error";
   res.status(error.status).json({
