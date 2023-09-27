87154d72d8caf83c6df6551f349829e6715918c8
mahraayush4321 <tanishqmahra@gmail.com>
2023-09-27 16:11:58 +0530

    upd

--- Controllers/messageController.js
+++ Controllers/messageController.js
@@ -1,16 +1,37 @@
 import MsgModel from "../Models/MessageModel.js";
+import redis from "../redis/redis.js";
+import { asyncHandler } from "../utils/asyncErrorhandler.js";
 
-export const addMessage = async (req, res) => {
+export const addMessage = asyncHandler(async (req, res, next) => {
   const { chatId, senderId, text } = req.body;
-  const message = new MsgModel({
-    chatId,
-    senderId,
-    text,
-  });
   try {
+    const message = new MsgModel({
+      chatId,
+      senderId,
+      text,
+    });
     const result = await message.save();
     res.status(200).json(result);
   } catch (error) {
-    res.status(500).json(error);
+    next(error);
   }
-};
+});
+
+//used redis cache->
+
+export const getMessage = asyncHandler(async (req, res, next) => {
+  const { chatId } = req.params;
+  try {
+    const cachedata = await redis.get(`chat:${chatId}`);
+    if (cachedata) {
+      console.log("cache hit for getting message", chatId);
+
+      return res.status(200).json(JSON.parse(cachedata));
+    }
+    const result = await MsgModel.find({ chatId });
+    await redis.setex(`chat:${chatId}`, 900, JSON.stringify(result));
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
